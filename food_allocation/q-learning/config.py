9744f45df6981fe297d8ccf07c4eb2405f47e5bf
lighthouse117 <lighthouse.happy@gmail.com>
2022-04-07 06:34:20 +0900

    勾配クリッピングで勾配爆発を抑える

--- food_allocation/q-learning/config.py
+++ food_allocation/q-learning/config.py
@@ -1,5 +1,5 @@
 class LearningParameters:
-    MAX_EPISODES = 6000001
+    MAX_EPISODES = 1000001
     MAX_STEPS = 100
     GREEDY_CYCLE = 1000
 
@@ -15,11 +15,18 @@ class LearningParameters:
 
 
 class EnvironmentSettings:
-    AGENTS_COUNT = 3
-    FOODS = [20, 20, 20]
+    # AGENTS_COUNT = 3
+    # FOODS = [20, 20, 20]
+    # NUM_FOODS = len(FOODS)
+    # REQUESTS = [
+    #     [10, 10, 10],
+    #     [5, 10, 5],
+    #     [5, 5, 10],
+    # ]
+
+    AGENTS_COUNT = 1
+    FOODS = [5, 5, 5]
     NUM_FOODS = len(FOODS)
     REQUESTS = [
-        [10, 10, 10],
-        [5, 10, 5],
-        [5, 5, 10],
+        [1, 3, 5],
     ]
